4dfc8a43ac4e2ff5e1eee39e2c3d13b797891545
M-ATEEK <muhammadateek.scholars@gmail.com>
2025-10-27 15:42:58 +0500

    Feature: User registration page

--- src/components/layouts/AuthLayouts.jsx
+++ src/components/layouts/AuthLayouts.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "../login/Index";
 import Navbar from "../navbar/Index";
+import Register from "../register/Index";
 
 class AuthLayout extends Component {
 	state = {};
@@ -12,6 +13,7 @@ class AuthLayout extends Component {
 				<Navbar />
 				<Switch>
 					<Route path='/login' component={Login} />
+					<Route path='/register' component={Register} />
 				</Switch>
 			</>
 		);
