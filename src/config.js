a6cfebfb1223dc561deb95d61b8b9a7df69cac52
faezeh <abdif701@gmail.com>
2025-10-31 15:22:30 +0500

    Feature: Facebook authentication integration

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 const API_URL = process.env.REACT_APP_API_URL;
 const IMG_URL = process.env.REACT_APP_IMG_URL;
-
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_FACEBOOK_APP_ID;
+const FACEBOOK_APP_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 export default {
-	API_URL,IMG_URL
+	API_URL,
+	IMG_URL,
+	GOOGLE_CLIENT_ID,
+	FACEBOOK_APP_ID,
 };
