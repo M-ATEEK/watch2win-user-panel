acc60a4c160d2d35a1866a099b7b4ce6538772d0
faezeh <abdif701@gmail.com>
2025-10-31 10:20:15 +0500

    Login,register,homepage api integration

--- src/components/home/inner-content/Index.jsx
+++ src/components/home/inner-content/Index.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Redirect } from "react-router";
 import userIcon from "../../../assets/images/user.png";
 import userSmallIcon from "../../../assets/images/userSm.png";
 import playIcon from "../../../assets/images/play.png";
@@ -8,50 +6,54 @@ import videoThumbnail from "../../../assets/images/video.png";
 import videoNameIcon from "../../../assets/images/videoNameIcon.png";
 import drillImage from "../../../assets/images/drillsImg.png";
 import heartIcon from "../../../assets/images/heart.png";
-import durationIcon from "../../../assets/images/durationIcon.png";
+
 import config from "../../../config";
 import axios from "axios";
-
+import { Redirect } from "react-router";
 import Auth from "../../Services/Auth";
+
 
 class InnerContent extends Component {
 	state = {
 		data: [],
-		page: 1,
-		totalItems: 0,
-		visible: 3,
+		drillsVideosData: [],
+		visible:1,
+		page:1
 	};
 
 	loadMore = this.loadMore.bind(this);
 	getDrillsData = () => {
-		axios
-			.get(`${config.API_URL}/admin/drills/?page=${this.state.page}`, {
+
+		axios.get(`${config.API_URL}/admin/drills/?page=${this.state.page}`, {
 				headers: {
-					Authorization: Auth.getToken(),
+					Authorization: Auth.getToken()
 				},
 			})
 			.then((response) => {
-				console.log("response " + JSON.stringify(response.data.data.drills));
 				this.setState({
-					data: [...this.state.data, ...response.data.data.drills],
-					page: this.state.page + 1,
-					totalItems: response.data.count,
+					data:[...response.data.data.drills],
+					drillsVideosData:[...response.data.data.drills],
 				});
 			});
 	};
 	componentDidMount() {
 		this.getDrillsData();
 	}
-	loadMore() {
+	loadMore(){
+		this.setState((old)=>{
+			return {
+				page:old.page+1
+			}
+		})
 		this.getDrillsData();
 	}
-	dateDifferenceInDays(date1, date2) {
+	dateDifferenceInDays(date1, date2){
 		const diffInMs = Math.abs(date1 - date2);
 
-		if (parseInt(diffInMs / (1000 * 60 * 60 * 24) > 7)) {
-			return "";
-		} else {
-			return (
+		if(parseInt(diffInMs / (1000 * 60 * 60 * 24) > 7)){
+			return '';
+		}else{
+			return (	
 				<li>
 					<a href='#' className='new'>
 						New
@@ -60,106 +62,93 @@ class InnerContent extends Component {
 			);
 		}
 	}
-	isPremimum(status) {
+	isPremimum(status){
 		console.log(status);
-		if (status == false) {
-			return "";
-		} else {
-			return (
+		if(status == false){
+			return '';
+		}else{
+			return(
 				<li>
 					<a href='#' className='premium'>
-						Premium
+						Premium 
 					</a>
 				</li>
 			);
 		}
 	}
-
-	addToFavourite = (drillId) => {
-		const response = {
-			"favouriteDrillVideos": drillId,
-			"isAdded": true
-		};
-		axios
-			.post(`${config.API_URL}/user/favoriteVideo`, response, {
-				headers: {
-					Authorization: Auth.getToken(),
-				},
-			})
-			.then((response) => {
-				console.log("response " + JSON.stringify(response.data.data.drills));
-
-			});
-	}
 	render() {
+		const videosCount = [1, 2, 3, 4];
 		const token = Auth.getToken();
 		if (!token) {
 			return <Redirect to='/login' />;
 		}
 		const data = this.state.data;
-		console.log("length" + data.length);
-		if (data.length > 0) {
+		const dataVideos = this.state.drillsVideosData;
+		if(data.length > 0){
 			return (
 				<div className='mainInnerContent'>
 					<div className='container'>
 						<div className='row'>
 							<div className='col-md-7 col-sm-8 col-xs-12 hidden-xs'>
-								{data.map((drills, index) => {
-									return (
-										<div key={index} className='videoMain form-group'>
-											<div className='videoHeader'>
-												<div className='row'>
-													<div className='col-md-1 col-sm-2 col-xs-2'>
-														<img src={userIcon} alt='' />
+						
+							{dataVideos.slice(0,this.state.page).map((drills, index) => {
+								
+								
+										return (
+											<div key={index} className='videoMain form-group'>
+												<div className='videoHeader'>
+													<div className='row'>
+														<div className='col-md-1 col-sm-2 col-xs-2'>
+															<img src={userIcon} alt='' />
+														</div>
+														<div className='col-md-11 col-sm-10 col-xs-9'>
+															<h4>{drills.athlete ? drills.athlete.name : 'Name Not Found'}</h4>
+														</div>
 													</div>
-													<div className='col-md-11 col-sm-10 col-xs-9'>
-														<h4>{drills.athlete ? drills.athlete.name : "Name Not Found"}</h4>
+												</div>
+												<div className='videoMainArea'>
+													<img src={videoThumbnail} alt='' />
+													<div className='videoPlay'>
+														<a href='#'>
+															<img src={playIcon} alt='' />
+														</a>
+													</div>
+													<div className='videoName'>
+														<img src={videoNameIcon} alt='' />
+														<span>Video name will show here </span>
+													</div>
+													<div className='videoSettings'>
+														<div className='col-md-6 col-sm-6 col-xs-6'>
+															<ul className='videoLeftSettings list-unstyled'>
+																<li>
+																	<a href='#' className='easy'>
+																	{drills.difficultyLevel ? drills.difficultyLevel.name : 'Name Not Found'}
+																	</a>
+																</li>
+															</ul>
+														</div>
+														<div className='col-md-6 col-sm-6 col-xs-6'>
+															<ul className='videoRightSettings list-unstyled'>
+																{this.dateDifferenceInDays(new Date(), new Date(drills.createdAt))}
+																
+																{this.isPremimum(drills.isPremium)}
+															</ul>
+														</div>
 													</div>
 												</div>
 											</div>
-											<div className='videoMainArea'>
-												<img src={videoThumbnail} alt='' />
-												<div className='videoPlay'>
-													<a href='#'>
-														<img src={playIcon} alt='' />
-													</a>
-												</div>
-												<div className='videoName'>
-													<img src={videoNameIcon} alt='' />
-													<span>Video name will show here </span>
-												</div>
-												<div className='videoSettings'>
-													<div className='col-md-6 col-sm-6 col-xs-6'>
-														<ul className='videoLeftSettings list-unstyled'>
-															<li>
-																<a href='#' className='easy'>
-																	{drills.difficultyLevel ? drills.difficultyLevel.name : "Name Not Found"}
-																</a>
-															</li>
-														</ul>
-													</div>
-
-													<div className='col-md-6 col-sm-6 col-xs-6'>
-														<ul className='videoRightSettings list-unstyled'>
-															{this.dateDifferenceInDays(new Date(), new Date(drills.createdAt))}
-															{this.isPremimum(drills.isPremium)}
-														</ul>
-													</div>
-												</div>
-											</div>
-										</div>
-									);
-								})}
-								<hr />
-								{this.state.totalItems < data.length && (
-									<div className='col-md-12 col-sm-12 col-xs-12 hidden-xs text-center'>
-
-										<button type='button' onClick={this.loadMore} className={"btn btn-sm btn-primary"}>
-											Load More
-										</button>
-
-									</div>
-								)}
+										);
+								
+								
+								
+							})}
+							<hr/>
+								<div className='col-md-12 col-sm-12 col-xs-12 hidden-xs text-center'>
+									{this.state.page < this.state.drillsVideosData.length &&
+									<button type="button" onClick={this.loadMore} className={"btn btn-sm btn-primary"}>Load More</button>
+									}
+								</div>
+							
 							</div>
 							<div className='col-md-5 col-sm-4 col-xs-12'>
 								<div className='drillsArea'>
@@ -168,70 +157,67 @@ class InnerContent extends Component {
 											<h3>Drills</h3>
 										</div>
 										<div className='col-md-6 col-sm-6 col-xs-6 text-right'>
-											<a href='/drills'>See All </a>
+											<a href='#'>See All </a>
 										</div>
 									</div>
 									<div className='drillsMain'>
-										{data.slice(0, this.state.visible).map((drills, index) => {
+									
+										{data.map((drills, index) => {
 											return (
-
 												<div key={index} className='videoMain form-group'>
-
 													<div className='videoMainArea'>
 														<img src={drillImage} alt='' />
 														<div className='videoName'>
 															<img src={videoNameIcon} alt='' />
 															<span>{drills.name} </span>
 														</div>
-
 														<div className='videoSettings'>
-															<Link to={`/drills/detail/${drills._id}`}>
-																<div className='col-md-6 col-sm-6 col-xs-6'>
-																	<ul className='videoLeftSettings uploader list-unstyled'>
+															<div className='col-md-6 col-sm-6 col-xs-6'>
+																<ul className='videoLeftSettings uploader list-unstyled'>
+															
 																		<li>
 																			<a href='#'>
-																				<img src={userSmallIcon} alt='' /> &nbsp;{" "}
-																				{drills.athlete ? drills.athlete.name : "Name Not Found"}
+																				<img src={userSmallIcon} alt='' /> &nbsp; {drills.athlete ? drills.athlete.name : 'Name Not Found'}
 																			</a>
 																		</li>
-																	</ul>
-																</div>
-															</Link>
+																	
+																
+																</ul>
+															</div>
 															<div className='col-md-6 col-sm-6 col-xs-6'>
 																<ul className='videoRightSettings favourite list-unstyled'>
 																	<li>
-																		<a href='#' onClick={() => this.addToFavourite(drills._id)}>
+																		<a href='#'>
 																			<img src={heartIcon} alt='' /> <span>222</span>
 																		</a>
 																	</li>
 																</ul>
 															</div>
 														</div>
-
 														<div className='durationSettings'>
 															<ul className='list-unstyled'>
-																<li>{drills.videos ? drills.videos.length : 0} Drills</li>
+																<li>{drills.videos.length} Drills</li>
 																<li>
-																	<img src={durationIcon} alt='' /> 12:41
+																	<img src='images/durationIcon.png' alt='' /> 12:41
 																</li>
 															</ul>
 														</div>
 													</div>
-
 												</div>
-
 											);
 										})}
 									</div>
 								</div>
+								
 							</div>
 						</div>
 					</div>
 				</div>
 			);
-		} else {
-			return "";
+		}else{
+			return '';
 		}
+	
 	}
 }
 
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const API_URL = process.env.REACT_APP_API_URL;
+const IMG_URL = process.env.REACT_APP_IMG_URL;
+
+export default {
+	API_URL,IMG_URL
+};
