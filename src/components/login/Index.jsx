1396cc4b3269e43853835a46dce321ba78b8d626
M-ATEEK <muhammadateek.scholars@gmail.com>
2025-11-01 12:25:08 +0500

    Login,register,homepage api integration

--- src/components/login/Index.jsx
+++ src/components/login/Index.jsx
@@ -1,22 +1,92 @@
 import React, { Component } from "react";
 import fbLogo from "../../assets/images/fbBtn.png";
 import googleLogo from "../../assets/images/gBtn.png";
+import config from "../../config";
+import axios from "axios";
+import { Redirect, Link } from "react-router-dom";
+import Joi from "joi";
+import Auth from "../Services/Auth";
 
 class Login extends Component {
-	state = {};
+	state = {
+		email: "",
+		password: "",
+		login: false,
+		errors: {},
+		message: "",
+		emptyfield: "",
+		auth: false,
+	};
+
+	schema = Joi.object({
+		email: Joi.string()
+			.email({ tlds: { allow: false } })
+			.required()
+			.label("Email"),
+		password: Joi.string().required().min(6).label("Password"),
+	});
+
+	submitHandler = (e) => {
+		e.preventDefault();
+		if (this.state.email === "" && this.state.password === "") {
+			this.setState({
+				message: "Enter email and password",
+			});
+		} else {
+			const data = this.state;
+			axios.post(`${config.API_URL}/authenticate`, data).then((response) => {
+				if (response.data.success) {
+					if (response.data.data.token !== undefined) {
+						Auth.setToken(response.data.data.token);
+						this.props.history.push("/home");
+					}
+				} else if (response.data.success === false) {
+					this.setState({
+						message: response.data.errors.email.message,
+					});
+				}
+			});
+		}
+	};
+
+	handleInput = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
 	render() {
+		if (localStorage.getItem("token")) {
+			return <Redirect to='/home' />;
+		}
 		return (
 			<div className='loginBg'>
 				<div className='container'>
 					<div className='loginArea'>
 						<h2>LOGIN</h2>
 						<div className='formArea'>
-							<form action='#' method='post'>
+						<form onSubmit={this.submitHandler} method='post'>
+									<p className='text-danger'>{this.state.message}</p>
 								<div className='form-group'>
-									<input type='email' className='form-control' placeholder='Enter Email' />
+									<input
+										type='email'
+										className='form-control'
+										placeholder='Email'
+										name='email'
+										value={this.state.email}
+										onChange={this.handleInput}
+									/>
 								</div>
 								<div className='form-group'>
-									<input type='password' className='form-control' placeholder='Enter Password' />
+									<input
+										type='password'
+										className='form-control'
+										placeholder='Password'
+										name='password'
+										onChange={this.handleInput}
+										value={this.state.password}
+									/>
+									{this.state.errors.password && <span className='text-danger'>{this.state.errors.password}</span>}
+											
 								</div>
 								<div className='form-group'>
 									<a href='#' className='forgotPass'>
