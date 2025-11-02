c3f19c3e0ab0f71b2cf65e95797fd2502be8948f
faezeh <abdif701@gmail.com>
2025-11-02 17:09:00 +0500

    Improvement: Mobile responsive design updates

--- src/components/subscription/inner-content/Index.jsx
+++ src/components/subscription/inner-content/Index.jsx
@@ -13,77 +13,71 @@ import Dumbel from "../../../assets/images/dumbel.png";
 class InnerContent extends Component {
 	state = {
 
-
+		data: []
 	};
+
+	getSubscriptionData = () => {
+
+		axios
+			.get(`${config.API_URL}/admin/subscription`, {
+				headers: {
+					Authorization: Auth.getToken(),
+				},
+			})
+			.then((response) => {
+				this.setState({
+					data: [...response.data.data.subscriptions],
+				});
+
+			});
+	};
+	componentDidMount() {
+		this.getSubscriptionData();
+	}
 
 	render() {
 		const settings = {
-			dots: false,
+			className: "center",
+			centerMode: true,
 			infinite: true,
-			speed: 500,
+			centerPadding: "60px",
 			slidesToShow: 3,
-			slidesToScroll: 1,
+			speed: 500
 		};
+		const data = this.state.data;
+
 
 		return (
 			<div className="mainInnerContent">
 				<div className="container">
 					<div className="packages">
 						<Slider  {...settings}>
-							<div className="col-md-12">
-								<div className="packageDesc">
-									<img src={Dumbel} alt="" />
-									<h3>Professional Plan</h3>
-									<p>Sed ut perspiciatis unde
-							  {/* <br> */}
-							   natus error sit  quae ab illo inventor</p>
-									{/* <hr> */}
-									<h2>$340/Month</h2>
-									<a href="#" className="btn btnUnFilled">Get Started</a>
-								</div>
-							</div>
-						</Slider>
-						<Slider  {...settings}>
-							<div className="col-md-12">
-								<div className="packageDesc">
-									<img src={Dumbel} alt="" />
-									<h3>Professional Plan</h3>
-									<p>Sed ut perspiciatis unde
-							  {/* <br> */}
-							   natus error sit  quae ab illo inventor</p>
-									{/* <hr> */}
-									<h2>$340/Month</h2>
-									<a href="#" className="btn btnUnFilled">Get Started</a>
-								</div>
-							</div>
-						</Slider>
-						<Slider  {...settings}>
-							<div className="col-md-12">
-								<div className="packageDesc">
-									<img src={Dumbel} alt="" />
-									<h3>Professional Plan</h3>
-									<p>Sed ut perspiciatis unde
-							  {/* <br> */}
-							   natus error sit  quae ab illo inventor</p>
-									{/* <hr> */}
-									<h2>$340/Month</h2>
-									<a href="#" className="btn btnUnFilled">Get Started</a>
-								</div>
-							</div>
-						</Slider>
-						<Slider  {...settings}>
-							<div className="col-md-12">
-								<div className="packageDesc">
-									<img src={Dumbel} alt="" />
-									<h3>Professional Plan</h3>
-									<p>Sed ut perspiciatis unde
-							  {/* <br> */}
-							   natus error sit  quae ab illo inventor</p>
-									{/* <hr> */}
-									<h2>$340/Month</h2>
-									<a href="#" className="btn btnUnFilled">Get Started</a>
-								</div>
-							</div>
+							{
+								(data.length > 0) ? (
+									[0, 1, 2].map(function () {
+										return data.map((subscription, index) => {
+											return (
+												<div key={index} className="col-md-12">
+													<div className="packageDesc">
+														<img src={Dumbel} alt="" />
+														<h3>{subscription.name}</h3>
+														<p>{subscription.details}</p>
+
+														<h2>${subscription.price}/Month</h2>
+														<Link className= "btn btnUnFilled" to="/payment">Get Started</Link>
+
+													</div>
+												</div>
+											);
+
+										})
+									})
+
+								) : (
+										<h2 style={{ color: "#fee6cc", textAlign: "center" }}>Subscription Not Found</h2>
+									)
+							}
+
 						</Slider>
 
 					</div>
