9bfbaf5998c389e8497fc6d7f2e907f53aad33cf
Muhammad Ateek <muhammadateek.scholars@gmail.com>
2025-11-02 17:44:00 +0500

    Feature: Payment system updates

--- src/components/payment/inner-content/Index.jsx
+++ src/components/payment/inner-content/Index.jsx
@@ -38,11 +38,10 @@ class InnerContent extends Component {
 		var start = moment(subscribeDetail.subscribeDate);
 		var current = moment().startOf('minute');
 		const duration = moment.duration(current.diff(start)).asDays()
-		console.log(duration)
-		if (duration <= "30") {
+	if(subscribeDetail.subscribe && duration<=30){
 			alert("subscribed");
 			window.location.href = "/subscription";
-		}
+		}else{	
 		axios.get(`${config.API_URL}/admin/subscription/${id}`, {
 			headers: {
 				Authorization: token,
@@ -61,6 +60,7 @@ class InnerContent extends Component {
 					clientToken: response.data.token,
 				});
 			})
+		}
 	}
 	async buy() {
 		// Send the nonce to your server
@@ -75,8 +75,10 @@ class InnerContent extends Component {
 					Authorization: Auth.getToken(),
 				},
 			}).then((response)=>{
+				console.log(response)
 				if(response.data.result.success){
 					alert('you subscribe successfully')
+					window.location.href="/home"
 				}
 				else{
 					alert('Error')
