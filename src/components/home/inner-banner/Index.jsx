6049630c430afa04f3c43ea2a03030b8090dea7e
Muhammad Ateek <muhammadateek.scholars@gmail.com>
2025-11-02 17:16:00 +0500

    Feature: Drills listing page

--- src/components/home/inner-banner/Index.jsx
+++ src/components/home/inner-banner/Index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import BasketBallImage from "../../../assets/images/b1.png";
 import Slider from "react-slick";
 import config from "../../../config";
 import axios from "axios";
 import { Redirect } from "react-router";
+import { data } from "jquery";
 import Auth from "../../Services/Auth";
 
 class InnerBanner extends Component {
@@ -13,12 +13,12 @@ class InnerBanner extends Component {
 	};
 
 	getAllData = () => {
-		axios
-			.get(`${config.API_URL}/admin/categories`, {
-				headers: {
-					Authorization: Auth.getToken(),
-				},
-			})
+
+		axios.get(`${config.API_URL}/admin/categories`, {
+			headers: {
+				Authorization: Auth.getToken(),
+			},
+		})
 			.then((response) => {
 				this.setState({
 					data: [...response.data.data.category],
@@ -27,9 +27,6 @@ class InnerBanner extends Component {
 	};
 	componentDidMount() {
 		this.getAllData();
-	}
-	drillsCategory(id) {
-		this.props.history.push(`/home:${this.state.userID}`);
 	}
 
 	render() {
@@ -41,35 +38,40 @@ class InnerBanner extends Component {
 			slidesToScroll: 1,
 		};
 		const token = Auth.getToken();
+		if (!token) {
+			return <Redirect to='/login' />;
+		}
 		const data = this.state.data;
 		if (data.length > 0) {
 			return (
 				<div className='innerBanner'>
 					<div className='container'>
 						<div className='innerBannerContent'>
+
 							<Slider {...settings}>
 								{data.map((category, i) => {
 									return (
 										<div key={i} className='bannerContent'>
-											<Link to={`/drills/category/${category._id}`}>
-												{category.image === undefined ? (
-													<img src={BasketBallImage} />
-												) : (
+											{category.image === undefined ? (
+												<img src={BasketBallImage} />
+											) : (
 													<img src={`${config.IMG_URL}/image/${category.image}`} />
 												)}
-												<h3>{category.name}</h3>
-											</Link>
+											<h3>{category.name}</h3>
 										</div>
-									);
-								})}
+									)
+
+								})
+								}
 							</Slider>
 						</div>
 					</div>
 				</div>
 			);
 		} else {
-			return "Record not found";
+			return 'Record not found';
 		}
+
 	}
 }
 
