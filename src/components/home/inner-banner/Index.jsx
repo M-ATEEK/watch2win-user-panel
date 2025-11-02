38b76d13878f4a849c82202a6a2a4d3ce551fe01
Muhammad Ateek <muhammadateek.scholars@gmail.com>
2025-11-02 17:06:00 +0500

    Bugfix: Resolve merge conflicts

--- src/components/home/inner-banner/Index.jsx
+++ src/components/home/inner-banner/Index.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
 import BasketBallImage from "../../../assets/images/b1.png";
 import Slider from "react-slick";
+import config from "../../../config";
+import axios from "axios";
+import { Redirect } from "react-router";
+import { data } from "jquery";
+import Auth from "../../Services/Auth";
 
 class InnerBanner extends Component {
-	state = {};
+	state = {
+		data: [],
+	};
+
+	getAllData = () => {
+
+		axios.get(`${config.API_URL}/admin/categories`, {
+				headers: {
+					Authorization: Auth.getToken(),
+				},
+			})
+			.then((response) => {
+				this.setState({
+					data:[...response.data.data.category],
+				});
+			});
+	};
+	componentDidMount() {
+		this.getAllData();
+	}
 
 	render() {
 		const settings = {
@@ -13,40 +37,41 @@ class InnerBanner extends Component {
 			slidesToShow: 4,
 			slidesToScroll: 1,
 		};
-		return (
-			<div className='innerBanner'>
-				<div className='container'>
-					<div className='innerBannerContent'>
-						<Slider {...settings}>
-							<div className='bannerContent'>
-								<img src={BasketBallImage} alt='' />
-								<h3>Basketball</h3>
+		const token = Auth.getToken();
+		if (!token) {
+			return <Redirect to='/login' />;
+		}
+		const data = this.state.data;
+			if (data.length > 0) {
+				return (
+					<div className='innerBanner'>
+						<div className='container'>
+							<div className='innerBannerContent'>
+								
+								<Slider {...settings}>	
+									{data.map((category, i) => {
+											return (
+												<div  key={i} className='bannerContent'>
+													{category.image === undefined ? (
+														<img src={BasketBallImage} />
+														) : (
+														<img src={`${config.IMG_URL}/image/${category.image}`}/>
+													)} 
+														<h3>{category.name}</h3>
+												</div>
+											)
+
+										})
+									}
+								</Slider>
 							</div>
-							<div className='bannerContent'>
-								<img src={BasketBallImage} alt='' />
-								<h3>Basketball</h3>
-							</div>
-							<div className='bannerContent'>
-								<img src={BasketBallImage} alt='' />
-								<h3>Basketball</h3>
-							</div>
-							<div className='bannerContent'>
-								<img src={BasketBallImage} alt='' />
-								<h3>Basketball</h3>
-							</div>
-							<div className='bannerContent'>
-								<img src={BasketBallImage} alt='' />
-								<h3>Basketball</h3>
-							</div>
-							<div className='bannerContent'>
-								<img src={BasketBallImage} alt='' />
-								<h3>Basketball</h3>
-							</div>
-						</Slider>
+						</div>
 					</div>
-				</div>
-			</div>
-		);
+				);
+		}else{
+			return 'Record not found';
+		}
+
 	}
 }
 
