30ef3460795a06846bae0187303a2bdbf4b7d684
Muhammad Ateek <muhammadateek.scholars@gmail.com>
2025-11-02 17:02:00 +0500

    Feature: Home page implementation

--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import MobileNavbar from "../Mobile-navbar/Index";
 import InnerBanner from "./inner-banner/Index";
 import InnerContent from "./inner-content/Index";
+
 class Home extends Component {
 	state = {};
+
 	render() {
-	
 		return (
 			<>
-
 				<InnerBanner />
 				<InnerContent />
 				<MobileNavbar />
--- src/components/home/inner-banner/Index.jsx
+++ src/components/home/inner-banner/Index.jsx
@@ -1,38 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import BasketBallImage from "../../../assets/images/b1.png";
 import Slider from "react-slick";
-import config from "../../../config";
-import axios from "axios";
-import { Redirect } from "react-router";
-import Auth from "../../Services/Auth";
-
-
 
 class InnerBanner extends Component {
-	state = {
-		data: [],
-	};
-
-	getAllData = () => {
-
-		axios.get(`${config.API_URL}/admin/categories`, {
-			headers: {
-				Authorization: Auth.getToken(),
-			},
-		})
-			.then((response) => {
-				this.setState({
-					data: [...response.data.data.category],
-				});
-			});
-	};
-	componentDidMount() {
-		this.getAllData();
-	}
-	drillsCategory(id) {
-		this.props.history.push(`/home:${this.state.userID}`)
-	}
+	state = {};
 
 	render() {
 		const settings = {
@@ -42,46 +13,40 @@ class InnerBanner extends Component {
 			slidesToShow: 4,
 			slidesToScroll: 1,
 		};
-		const token = Auth.getToken();
-		if (!token) {
-			return <Redirect to='/login' />;
-		}
-		const data = this.state.data;
-		if (data.length > 0) {
-			return (
-				<div className='innerBanner'>
-					<div className='container'>
-						<div className='innerBannerContent'>
-
-							<Slider {...settings}>
-								{data.map((category, i) => {
-									return (
-										<div key={i} className='bannerContent'>
-											<Link to={`/drills/category/${category._id}`}>
-
-
-												{category.image === undefined ? (
-													<img src={BasketBallImage} />
-												) : (
-														<img src={`${config.IMG_URL}/image/${category.image}`} />
-													)}
-												<h3>{category.name}</h3>
-
-											</Link>
-										</div>
-									)
-
-								})
-								}
-							</Slider>
-						</div>
+		return (
+			<div className='innerBanner'>
+				<div className='container'>
+					<div className='innerBannerContent'>
+						<Slider {...settings}>
+							<div className='bannerContent'>
+								<img src={BasketBallImage} alt='' />
+								<h3>Basketball</h3>
+							</div>
+							<div className='bannerContent'>
+								<img src={BasketBallImage} alt='' />
+								<h3>Basketball</h3>
+							</div>
+							<div className='bannerContent'>
+								<img src={BasketBallImage} alt='' />
+								<h3>Basketball</h3>
+							</div>
+							<div className='bannerContent'>
+								<img src={BasketBallImage} alt='' />
+								<h3>Basketball</h3>
+							</div>
+							<div className='bannerContent'>
+								<img src={BasketBallImage} alt='' />
+								<h3>Basketball</h3>
+							</div>
+							<div className='bannerContent'>
+								<img src={BasketBallImage} alt='' />
+								<h3>Basketball</h3>
+							</div>
+						</Slider>
 					</div>
 				</div>
-			);
-		} else {
-			return 'Record not found';
-		}
-
+			</div>
+		);
 	}
 }
 
